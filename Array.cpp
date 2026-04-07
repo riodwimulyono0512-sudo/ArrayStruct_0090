d2f9558b606aca8b7c7ad070f68ebec3cea90095
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-07 14:56:31 +0700

    tanpa inisialisasi

--- Array.cpp
+++ Array.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main(){
     //Deklarasi Array dengan inisialisasi
     int dataku[5] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    int datamu[5];
+    int x;
 }
