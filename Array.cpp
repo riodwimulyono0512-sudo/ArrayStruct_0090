8363390b60fe82c9d82bf6b8c083fa3e1618432a
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-07 15:10:53 +0700

    tampilkan arrray satu per satu

--- Array.cpp
+++ Array.cpp
@@ -27,4 +27,11 @@ int main(){
     cin >> datamu[3];
     cout << "Data Index ke - 4 : " ;
     cin >> datamu[4];
+
+    //Tampilkan array datamu satu per satu
+    cout << "Data ke - 1 : " << datamu[0]<<endl;
+    cout << "Data ke - 2 : " << datamu[1]<<endl;
+    cout << "Data ke - 3 : " << datamu[2]<<endl;
+    cout << "Data ke - 4 : " << datamu[3]<<endl;
+    cout << "Data ke - 5 : " << datamu[4]<<endl;
 }
