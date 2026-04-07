9b8501bb15783f3f6abb23c24d8eed17664f4f1c
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-07 15:08:27 +0700

    mengisi array datamu satu per satu

--- Array.cpp
+++ Array.cpp
@@ -15,4 +15,16 @@ int main(){
     //cara menulis atau mengisi rray
     dataku[2] = 600;
     cout << "Data ke - 3 yang baru : " <<dataku[2]<<endl;
+
+    //Mengisi array datamu satu per satu
+    cout << "Data Index ke - 0 : " ;
+    cin >> datamu[0];
+    cout << "Data Index ke - 1 : " ;
+    cin >> datamu[1];
+    cout << "Data Index ke - 2 : " ;
+    cin >> datamu[2];
+    cout << "Data Index ke - 3 : " ;
+    cin >> datamu[3];
+    cout << "Data Index ke - 4 : " ;
+    cin >> datamu[4];
 }
