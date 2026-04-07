cae300395845917d0a35f5b285d6364eb7819544
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-07 15:46:58 +0700

    finish

--- StructGetline.cpp
+++ StructGetline.cpp
@@ -10,3 +10,23 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+int main()
+{
+    //Deklarasi obyek dari struct orang
+    orang mhs;
+    //isi objek
+    cout << "Nama Mahasiswa:";
+    getline (cin, mhs.nama);
+    cout << "Alamat Desa:";
+    getline (cin, mhs.alamat.desa);
+    cout << "Alamat Kota:";
+    getline (cin, mhs.alamat.kota);
+    cout << "Umur:";
+    cin >> mhs.umur;
+    cout << endl;
+    //Menampilkan isi obyek
+    cout << "Nama : " << mhs.nama<<endl;
+    cout << "Desa : " << mhs.alamat.desa<<endl;
+    cout << "Kota : " << mhs.alamat.kota<<endl;
+    cout << "Umur : " << mhs.umur<<endl;
+}
